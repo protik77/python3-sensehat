e7020fc379cb424f71d23734e6aed46981cb0f77
Protik Das <protik77@gmail.com>
2019-05-24 13:39:21 -0700

    update test code

--- test.py
+++ test.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     temperature = sense.get_temperature_from_humidity()
     print("Current temperature: {} C\n".format(temperature))
 
-    sense.show_message("successfully installed!")
+    sense.show_message("It worked!")
